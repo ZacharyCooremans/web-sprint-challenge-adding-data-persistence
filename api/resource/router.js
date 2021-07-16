f71b94c4538ce1cdc6291e6c255e7ee1ea937b7c
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 12:20:16 -0500

    migration tables done

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,9 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+const Resource = require('./model')
+
+router.get('/', (req, res, next) => {
+    res.json({message: 'goti it adasfd from resource'})
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,2 +1,8 @@
 // build your `/api/tasks` router here
 const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+    res.json({message: 'goti it from task'})
+})
+
+module.exports = router
