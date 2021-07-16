88b304af96dd59445b1be805bcc9a40fd5fe44ab
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 15:09:01 -0500

    test2

--- data/migrations/20210716170627_project.js
+++ data/migrations/20210716170627_project.js
@@ -18,7 +18,7 @@ exports.up = async function(knex) {
             table.increments('task_id')
             table.string('task_description').notNullable()
             table.string('task_notes')
-            table.boolean('task_completed').defaultTo(false)
+            table.boolean('task_completed').defaultTo(0)
             table.integer('project_id')
                 .unsigned()
                 .notNullable()
