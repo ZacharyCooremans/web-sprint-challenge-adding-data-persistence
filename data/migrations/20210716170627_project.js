9862d9fa5d5e62a9d1255a3577b14e28a81a2311
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 15:14:56 -0500

    test3

--- data/migrations/20210716170627_project.js
+++ data/migrations/20210716170627_project.js
@@ -5,7 +5,7 @@ exports.up = async function(knex) {
             table.increments('project_id')
             table.string('project_name', 100).notNullable()
             table.string('project_description')
-            table.boolean('project_completed').defaultTo(false)
+            table.boolean('project_completed').defaultTo(0)
         })
 
         .createTable('resources', table => {
@@ -18,7 +18,7 @@ exports.up = async function(knex) {
             table.increments('task_id')
             table.string('task_description').notNullable()
             table.string('task_notes')
-            table.boolean('task_completed').defaultTo(0)
+            table.boolean('task_completed').defaultTo(false)
             table.integer('project_id')
                 .unsigned()
                 .notNullable()
