d3043142413ba24df059a54720085c3e71380258
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 14:58:26 -0500

    just need to fixe post

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,18 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const getTasks = () => {
+    return db('tasks')
+}
+
+const addTask = (task) => {
+    return db('tasks')
+        .insert(task, 'task_id')
+        .then(ids => ({task_id: ids[0]}))
+}
+
+module.exports = {
+    getTasks,
+    addTask
+
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,8 +1,26 @@
 // build your `/api/tasks` router here
 const router = require('express').Router()
+const Task = require('./model')
 
-router.get('/', (req, res, next) => {
-    res.json({message: 'goti it from task'})
+router.get('/', (req, res) => {
+    Task.getTasks()
+        .then(tasks => {
+            res.status(200).json(tasks)
+        })
+        .catch()
+})
+
+router.post('/', (req, res) => {
+    const task = req.body
+    Task.addTask(task)
+        .then(task => {
+            res.status(201).json(task)
+        })
+        .catch(err => {
+            err.status(500).json({
+                message: err.message
+            })
+        })
 })
 
 module.exports = router
