802cf00656bb8fc374ce2bb427396f2a8cc1ef64
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 11:34:38 -0500

    got server up

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,9 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+router.get('/', (req, res, next) => {
+    res.json({message: 'goti it'})
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express')
+const projectRouter = require('./project/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/project', projectRouter)
+
+server.use('*', (req,res) => {
+    res.json({api: 'up'})
+})
+
+module.exports = server
