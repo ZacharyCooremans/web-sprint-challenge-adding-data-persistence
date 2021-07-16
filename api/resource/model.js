79dd25585d475c0e4ee4efcc980e0c95c159b6e0
Zachary Cooremans <zcooremans@gmail.com>
2021-07-16 14:18:41 -0500

    got resouce working I hope

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const getResources = () => {
+    return db('resources')
+}
+
+const addResources = (resource) => {
+    return db('resources')
+        .insert(resource, 'resource_id')
+        .then(ids => ({ id: ids[0]}))
+}
+
+module.exports = {
+    getResources,
+    addResources
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -2,8 +2,29 @@
 const router = require('express').Router()
 const Resource = require('./model')
 
-router.get('/', (req, res, next) => {
-    res.json({message: 'goti it adasfd from resource'})
+router.get('/', (req, res) => {
+    Resource.getResources()
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(err => {
+            err.status(500).json({
+                message: err.message
+            })
+        }) 
+})
+
+router.post('/', (req, res) => {
+    const resource = req.body
+    Resource.addResources(resource)
+        .then(resource => {
+            res.status(201).json(resource)
+        })
+        .catch(err => {
+            err.status(500).json({
+                message: err.message
+            })
+        })
 })
 
 module.exports = router
